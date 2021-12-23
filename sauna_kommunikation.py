4bc65f734abed2e4ace2052b52b9e1abb6bae30d
Quirin Teunis <jundarer@gmail.com>
2021-12-23 20:22:30 +0100

    Automatisierung von der Existenz der RPi lib damit brauch man kein ausklammern mehr

--- sauna_kommunikation.py
+++ sauna_kommunikation.py
@@ -1,6 +1,14 @@
-#from RPi import GPIO # geht NUR im Raspi!
+import os
+
+debug=False
+try:
+    from RPi import GPIO
+except ImportError:
+    print("Modul wird im Debug-Modus gestartet, da das RPi Modul nicht gefunden wurde")
+    debug=True
 
 class sauna:
+    """Steuert die direkte Kommunikation mit der Sauna"""
     def __init__(self):
         # GPIO Variablen
         self.Rel_out = (31, 33, 35, 37)  # GPIO-Pins (N, R, S, T)
@@ -18,19 +26,19 @@ class sauna:
     def init_GPIO(self):
         """Konfiguriert die GPIO pins"""
         # GPIO konfig
-        # GPIO.setmode(GPIO.BOARD)
-        # GPIO.setwarnings(False)
-        # for i in Rel_out:
-        #     GPIO.setup(i, GPIO.OUT)
-        #     GPIO.output(i, False) # Am Anfang alles auf Null!
-        # nun sind die out-pins konfiguriert!
+        if debug:
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setwarnings(False)
+            for i in self.Rel_out:
+                GPIO.setup(i, GPIO.OUT)
+                GPIO.output(i, False) # Am Anfang alles auf Null!
+            # nun sind die out-pins konfiguriert!
 
-        # Ermitteln des Temperatur-Files (Seite 315) gilt NUR für den PI!
-        # und der Sensor MUSS angeschlossen sein!
-        #os.system("modprobe wire")      # modprobe nicht nötig
-        #os.system("modprobe w1-gpio")   # ist in boot-config festgelegt
-        #os.system("modprobe w1-therm")
-        return
+            # Ermitteln des Temperatur-Files (Seite 315) gilt NUR für den PI!
+            # und der Sensor MUSS angeschlossen sein!
+            os.system("modprobe wire")      # modprobe nicht nötig
+            os.system("modprobe w1-gpio")   # ist in boot-config festgelegt
+            os.system("modprobe w1-therm")
     
     def starten(self, aktuelleTemp, sollTemp):
         """Startet die Sauna mit der gegeben Ziel Temperatur"""
